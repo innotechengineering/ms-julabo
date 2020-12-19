375d33d2d7be8bf9d517d6c7c9649c3d494d5619
tmcolby <tyson.colby@innotech.engineering>
2020-12-19 12:22:26 -0800

    Initial

---
+++ julabo.py
@@ -0,0 +1,11 @@
+
+#!/usr/bin/env python3
+import serial
+
+#standard python logging
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+def Julabo(Object):
+    pass
