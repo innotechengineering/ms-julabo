116fa7bb6d7ace02da30ed88fb518e0d84a6edb8
tmcolby <tyson.colby@innotech.engineering>
2020-12-21 10:31:55 -0800

    First laydown of code

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ms-julabo
 Serial communication to Julabo Diamond Sous Vide
+
+Initial implementation of RS232 control interface - UNTESTED
+
+#### Notes:
+Currently `rtscts=False` - docs say that hardware handshaking is required.  we wont know until we connect (most devices will work ok with software or automatic flow control).  if we have trouble with handshaking, we can set `rtscts=True`.  Additionally, I have provided a multipurpose adapter cable where we have CTS and RTS physically jumpered.  
+
+Not totally sure of the DB9 sex on the Jualbo machine.  The multipurpose adapter cable will allow us to convert to any possible permutation.  
+
+Not totally sure if a true null modem connection is required.  The multipurpose adapter cable has crossover connections should we need them.  
--- julabo.py
+++ julabo.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python3
 import serial
 
@@ -8,4 +7,75 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 def Julabo(Object):
-    pass
+    def __init__(self):
+        self.sc = serial.Serial(port='/dev/ttyUSB0',
+                                baudrate=4800,
+                                bytesize=serial.SEVENBITS,
+                                parity=serial.PARITY_EVEN,
+                                stopbits=serial.STOPBITS_ONE,
+                                timeout=5,
+                                xonxoff=False,                   # software flow control
+                                rtscts=False,                    # enable hardware (RTS/CTS) flow control
+                                write_timeout=None,              
+                                dsdtr=False,                     # enable hardware (DSR/DTR) flow control
+                                inter_byte_timeout=None,
+                                exclusive=None)
+        self.sc.reset_input_buffer()
+        self.sc.reset_output_buffer()
+
+    def rs232_control(self): 
+        # command to set the Diamond into rs232 control
+        self.sc.write("**REMOTE".encode('utf-8'))
+        print(self.sc.readline())  # may need to clear the input buffer is the julabo responds here ???
+
+    def dump_datalog(self):
+        self.sc.write("**DATALOGGER_OUT".encode('utf-8'))
+        # TODO - loop through reads here to drain datalog memeory - not sure yet
+
+    def _write_then_read(self, strToSend):
+        self.sc.write(strToSend.encode('uft-8'))
+        resp = self.sc.readline()
+        return resp
+
+    def read_version(self):
+        return self._write_then_read("version")
+
+    def read_status(self):
+        return self._write_then_read("status")
+
+    def read_bath_temp(self):
+        return self._write_then_read("in_pv_00")
+
+    def read_heating_power(self):
+        return self._write_then_read("in_pv_01")
+
+    def read_pt100(self):
+        return self._write_then_read("in_pv_02")
+
+    def read_saftey_sensor(self):
+        return self._write_then_read("in_pv_03")
+    
+    def read_working_temp(self):
+        return self._write_then_read("in_sp_00")
+
+    def read_temp_units(self):
+        return self._write_then_read("in_sp_02")
+
+    def read_circulator_status(self):
+        return self._write_then_read("in_mode_05")
+
+    def start_circulator(self):
+        return self._write_then_read("out_mode_05 1")
+
+    def stop_circulator(self):
+        return self._write_then_read("out_mode_05 0")
+
+    def set_working_temp(self, setTemp):
+        """for now, setTemp should be a string xxx.xx"""
+        return self._write_then_read("out_sp_00 " + setTemp)
+
+if __name__ == "__main__":
+    julabo = Julabo()
+    julabo.rs232_control()
+    ver = jualbo.read_version()
+    print(ver)
