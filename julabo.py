674d57ee715c0c5a41e6b94ac3aa6813f2777e17
tmcolby <tyson.colby@innotech.engineering>
2021-01-04 10:10:20 -0800

    Full working
    Write commands working.

--- julabo.py
+++ julabo.py
@@ -15,7 +15,7 @@ class Julabo(object):
                                 stopbits=serial.STOPBITS_ONE,
                                 timeout=5,
                                 xonxoff=False,                   # software flow control
-                                rtscts=True,                    # enable hardware (RTS/CTS) flow control
+                                rtscts=True,                     # enable hardware (RTS/CTS) flow control
                                 write_timeout=None,              
                                 dsrdtr=True,                     # enable hardware (DSR/DTR) flow control
                                 inter_byte_timeout=None,
@@ -26,13 +26,22 @@ class Julabo(object):
     def close(self):
         self.sc.close()
 
-    def rs232_control(self): 
-        # command to set the Diamond into rs232 control
-        self.sc.write("**REMOTE".encode('utf-8'))
-        print(self.sc.readline())  # may need to clear the input buffer is the julabo responds here ???
+    def remote_control(self): 
+        """ idempotent remote control command """
+        status = self.read_status().split()[1]
+        if status != "REMOTE":
+            self.sc.write("**REMOTE".encode('utf-8'))
+
+    def local_control(self): 
+        """ idempotent local control command """
+        status = self.read_status().split()[1]
+        if status != "MANUAL":
+            self.sc.write("**REMOTE".encode('utf-8'))
 
     def dump_datalog(self):
-        self.sc.write("**DATALOGGER_OUT".encode('utf-8'))
+        """ not implemented yet """
+        pass
+        # self.sc.write("**DATALOGGER_OUT".encode('utf-8'))
         # TODO - loop through reads here to drain datalog memeory - not sure yet
 
     def _write_then_read(self, strToSend):
@@ -62,20 +71,26 @@ class Julabo(object):
         return self._write_then_read("in_sp_00\r")
 
     def read_temp_units(self):
-        return self._write_then_read("IN_SP_06\r")
+        tempUnits = ["C", "F"]
+        res = self._write_then_read("IN_SP_06\r")
+        return tempUnits[int(res)]
 
     def read_circulator_status(self):
-        return self._write_then_read("in_mode_05\r")
+        status = ["STOPPED", "RUNNING"]
+        res = self._write_then_read("in_mode_05\r")
+        return status[int(res)]
 
     def start_circulator(self):
-        return self._write_then_read("out_mode_05 1\r")
+        self.sc.write(b"out_mode_05 1\r")
+        return self.read_status()
 
     def stop_circulator(self):
-        return self._write_then_read("out_mode_05 0\r")
+        self.sc.write(b"out_mode_05 0\r")
+        return self.read_status()
 
     def set_working_temp(self, setTemp):
-        """for now, setTemp should be a string xxx.xx"""
-        return self._write_then_read("out_sp_00 " + setTemp + '\r')
+        self.sc.write(f"out_sp_00 {str(setTemp)}\r".encode('utf-8'))
+        return self.read_working_temp()
 
 if __name__ == "__main__":
     julabo = Julabo()
@@ -83,18 +98,27 @@ if __name__ == "__main__":
     ver = julabo.read_version()
     print(f"Version: {ver}")
     print(f"Status: {julabo.read_status()}")
-    circulatorStates = ["STOPPED", "RUNNING"]
-    circulatorStatus = circulatorStates[int(julabo.read_circulator_status())]
-    print(f"Circulator status: {circulatorStatus}")
-    tempUnits = ["C", "F"]
-    tempUnits = tempUnits[int(julabo.read_temp_units())]
-    print(f"Working temp (setpoint): {julabo.read_working_temp()}{tempUnits}")
-    bathTemp = julabo.read_bath_temp()
-    print(f"Actual Bath temp: {bathTemp}{tempUnits}")
-    heatingPower = julabo.read_heating_power()
-    print(f"Heating power: {heatingPower}%")
     print()
-    print(f"Saftey sensor: {julabo.read_saftey_sensor()}{tempUnits}")
+
+    def print_status():
+        print(f"Circulator status: {julabo.read_circulator_status()}")
+        tempUnits = julabo.read_temp_units()
+        print(f"Working temp (setpoint): {julabo.read_working_temp()}{tempUnits}")
+        bathTemp = julabo.read_bath_temp()
+        print(f"Actual Bath temp: {bathTemp}{tempUnits}")
+        heatingPower = julabo.read_heating_power()
+        print(f"Heating power: {heatingPower}%")
+
+    julabo.local_control()
+    wt = julabo.set_working_temp(57.7)
+    print(wt)
+    print_status()
+
+    wt = julabo.set_working_temp(55.55)
+    print(wt)
+
+    julabo.stop_circulator()
+    print_status()
 
 
     
